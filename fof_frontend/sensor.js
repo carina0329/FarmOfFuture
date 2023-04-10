787b30a3c397c09ed7b0d7614dcf0a2bff3cc647
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-04-10 13:57:37 -0500

    fixed sensor data by adding site and plot

--- fof_frontend/sensor.js
+++ fof_frontend/sensor.js
@@ -1,35 +1,56 @@
 const sensor={template: `
 <div style="display: flex; flex-direction: column; align-items: center;">
-  <canvas id="sensor"></canvas>
-  <div style="display: flex; justify-content: center; align-items: center; margin-top: 1rem;">
-    <input type="text" id="inputBox" v-model="date_1" placeholder="Start Date" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
-    <input type="text" id="inputBox2" v-model="date_2" placeholder="End Date" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
-    <input type="text" id="inputBox3" v-model="depth" placeholder="Depth" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
-    <button @click="renderLineGraph(date_1, date_2, depth)" style="background-color: #4CAF50; color: white; padding: 0.5rem; border: none; border-radius: 5px;">Get Soil Data</button>
-  </div>
+    <canvas id="sensor"></canvas>
+    <div v-if="loading" style="display: flex; justify-content: center; align-items: center; margin-top: 1rem;">
+        <p>Loading...</p>
+    </div>
+    <div v-else style="display: flex; justify-content: center; align-items: center; margin-top: 1rem;">
+        <input type="text" id="inputBox" v-model="date_1" placeholder="Start Date" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
+        <input type="text" id="inputBox2" v-model="date_2" placeholder="End Date" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
+        <select v-model="depth" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
+            <option disabled value="">Depth</option>
+            <option v-for="depth in depth_list" :value="depth">{{depth}}</option>
+        </select>
+        <select v-model="site" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
+            <option disabled value="">Site</option>
+            <option v-for="site in site_list" :value="site">{{site}}</option>
+        </select>
+        <select v-model="plot" style="margin-right: 1rem; padding: 0.5rem; border: 1px solid #ccc; border-radius: 5px;">
+            <option disabled value="">Plot</option>
+            <option v-for="plot in plot_list" :value="plot">{{plot}}</option>
+        </select>
+        <button @click="renderLineGraph(date_1, date_2, site, plot, depth)" style="background-color: #4CAF50; color: white; padding: 0.5rem; border: none; border-radius: 5px;">Get Soil Data</button>
+    </div>
 </div>
-
 `,
 data() {
     return{
         sensorData:[],
-        depthToVal: {},
+        depth_list: [],
+        date_to_depthToVal: {},
         date_list: [],
+        plot_list: [],
+        site_list: [],
         date_1: null,
         date_2: null,
         depth: null,
         chartObj: null,
         canvas: null,
+        loading: true,
+        site: null,
+        plot: null,
     }
 },
 methods:{
     async initializeLineGraph(){
+        this.loading = true;
         await this.refreshData();
         // get the latest date available
         const last10 = this.date_list.slice(-10)
-        this.renderLineGraph(last10[0], last10[9], 10)
+        this.renderLineGraph(last10[0], last10[9], "energyFarm", "MaizeControl", 20)
+        this.loading = false;
     },
-    renderLineGraph(date_1, date_2, depth){
+    renderLineGraph(date_1, date_2, site, plot, depth){
         // label
         // let date_list = []
         // let depthToVal = {}
@@ -41,12 +62,22 @@ methods:{
                 label_lst.push(this.date_list[idx]);
             }
         }
+        // construct dataset to display
+        dataset_sliced = {}
+        value_array_render = []
+        for(label in label_lst){
+            console.log(label_lst[label])
+            console.log(this.date_to_depthToVal[label_lst[label]])
+            dataset_sliced = this.date_to_depthToVal[label_lst[label]]
+            value_array_render.push(dataset_sliced[site][plot][depth][0])
+        }
+        console.log(value_array_render)
         const data = {
             labels: label_lst,
             datasets: [
                 {
                     label: depth,
-                    data: this.depthToVal[depth],
+                    data: value_array_render,
                     yAxisID: 'y',
                 },
             ]
@@ -91,29 +122,53 @@ methods:{
     async refreshData(){
         await axios.get("http://127.0.0.1:8000/viewsensordata")
         .then((response) => {
-            this.sensorData = response.data;
+            var sensorData = response.data;
             // console.log(this.sensorData);
-            console.log(this.sensorData.length);
-            if(this.sensorData.length <= 0) {
+            console.log(sensorData.length);
+            if(sensorData.length <= 0) {
                 throw new Error('Sensor Data is none!');
             }
-            for (let i = 0; i < this.sensorData.length; ++i) {
-            date = this.sensorData[i].Date
-            if(this.date_list.indexOf(date) === -1) {
-                this.date_list.push(date);
+            for (let i = 0; i < sensorData.length; ++i) {
+                date = sensorData[i].Date
+                if(!(date in this.date_to_depthToVal)) {
+                    this.date_list.push(date);
+                    this.date_to_depthToVal[date] = {}
+                    // depthToVal_tmp = {}
+                }
+                let site = sensorData[i].Site
+                let plot = sensorData[i].Plot
+                let value = sensorData[i].Value
+                // console.log(value);
+                let depth = sensorData[i].Depth;
+                // console.log(depth);
+                if(this.site_list.indexOf(site) === -1) {
+                    console.log(site)
+                    this.site_list.push(site);
+                }
+                if(this.plot_list.indexOf(plot) === -1) {
+                    this.plot_list.push(plot);
+                }
+                if(this.depth_list.indexOf(depth) === -1) {
+                    this.depth_list.push(depth);
+                }
+                
+                if(!(site in this.date_to_depthToVal[date])) {
+                    this.date_to_depthToVal[date][site] = {};
+                }
+                if(!(plot in this.date_to_depthToVal[date][site])) {
+                    this.date_to_depthToVal[date][site][plot] = {};
+                }
+                if(depth in this.date_to_depthToVal[date]){
+                    this.date_to_depthToVal[date][site][plot][depth].push(value);
+                }
+                else{
+                    this.date_to_depthToVal[date][site][plot][depth] = [value];
+                }
+                // this.date_to_depthToVal[date] = depthToVal_tmp;
             }
-            let value = this.sensorData[i].Value
-            // console.log(value);
-            let depth = this.sensorData[i].Depth;
-            // console.log(depth);
-            if(depth in this.depthToVal){
-                this.depthToVal[depth].push(value);
-            }
-            else{
-                this.depthToVal[depth] = [value];
-            }
-        }       
+            this.depth_list.sort();
     });
+    
 },
 },
 mounted:function(){
